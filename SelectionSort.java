256178dd52af02accd31bb6f2d55eb8a42261e7a
Ilya Balatsenko <senkoet@gmail.com>
2025-10-31 07:54:03 -0400

    Added subsorts for insertion and selection for sorting within a range

--- SelectionSort.java
+++ SelectionSort.java
@@ -23,6 +23,56 @@ public class SelectionSort {
     }
 
 
+    /**
+     * Performs a selection sort on the provided array.
+     * This implementation is based on the OpenDSA textbook version,
+     * adapted for Comparable items.
+     */
+    public static <T extends Comparable<T>> void providedSelectionSort(T[] items) {
+
+        for (int i = 0; i <= items.length - 1; i++) { // Select i'th biggest record
+            int bigindex = 0; // Current biggest index
+            for (int j = 1; j < items.length - i; j++) { // Find the max value
+                if (items[j].compareTo(items[bigindex]) > 0) { // Found something bigger
+                    bigindex = j; // Remember bigger index
+                }
+            }
+            swap(items, bigindex, items.length - i - 1); // Put it into place
+        }
+    }
+
+    /**
+     * Swaps elements with index i and index j in the provided array.
+     */
+    public static <T extends Comparable<T>> void swap(T[] items, int i, int j) {
+        T tmp = items[i];
+        items[i] = items[j];
+        items[j] = tmp;
+    }
+
+
+    public static <T extends Comparable<T>> void selectSubsort(T[] items, int start, int end) {
+
+        // Outer loop: move the boundary of the unsorted subrange leftward
+        for (int i = end; i > start; i--) {
+            int bigindex = start; // assume first element in subrange is largest
+
+            // Find index of the largest element in [start..i]
+            for (int j = start + 1; j <= i; j++) {
+                if (items[j].compareTo(items[bigindex]) > 0) {
+                    bigindex = j;
+                }
+            }
+
+            // Swap the found largest element with the element at position i
+            swap(items, bigindex, i);
+        }
+    }
+
+
+
+
+
 
 
     public static void main(String[] args) {
--- insertionSort.java
+++ insertionSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class InsertionSort {
 
     public static void insertionSort(int[] array) {
@@ -20,6 +22,53 @@ public class InsertionSort {
     }
 
 
+    /**
+     * Performs an insertion sort on the provided array.
+     * Based on the OpenDSA textbook version, adapted for Comparable items.
+     */
+    public static <T extends Comparable<T>> void providedInsertionSort(T[] items) {
+
+        for (int i = 1; i < items.length; i++) {
+            T key = items[i];
+            int j = i - 1;
+
+            // Move elements of items[0..i-1] that are greater than key
+            // one position ahead of their current position
+            while (j >= 0 && items[j].compareTo(key) > 0) {
+                items[j + 1] = items[j];
+                j--;
+            }
+            items[j + 1] = key;
+        }
+    }
+
+
+    /**
+     * Performs an insertion sort only within the specified subrange of the array.
+     * The subrange is defined by start and end indices (both inclusive).
+     *
+     * @param items the array containing the subrange to be sorted
+     * @param start the starting index of the subrange (inclusive)
+     * @param end   the ending index of the subrange (inclusive)
+     */
+    public static <T extends Comparable<T>> void insertSubsort(T[] items, int start, int end) {
+        for (int i = start + 1; i <= end; i++) {
+            T key = items[i];
+            int j = i - 1;
+
+            
+            while (j >= start && items[j].compareTo(key) > 0) {
+                items[j + 1] = items[j];
+                j--;
+            }
+            items[j + 1] = key;
+        }
+    }
+
+
+
+
+
 
     public static void main(String[] args) {
 
@@ -30,6 +79,12 @@ public class InsertionSort {
         for (int i : array) {
             System.out.print(i + " ");
         }
+
+        Integer[] arr = {9, 4, 2, 8, 1, 6, 5};
+        insertSubsort(arr, 1, 5);
+
+        System.out.print(Arrays.toString(arr));
+        // Expected output: [9, 1, 2, 4, 6, 8, 5]
     }
     
 }
